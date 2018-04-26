a258ad8b92c4635018b800d27025bea1db3dccd9
lijialiang <lijialiang@yy.com>
2018-04-26 09:47:24 +0800

    [update] v0.0.10 ^upgrade babel 7 beta 46

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+## Unreleased
+
+* [null]
+
+## 0.0.10 (April 24, 2018)
+
+* 升级 Babel 7 beta.46
+
+## 0.0.9 (April 24, 2018)
+
+* 修复 Windows 兼容性问题
--- core/common/babel_options.js
+++ core/common/babel_options.js
@@ -3,7 +3,7 @@
 module.exports = {
     presets: [
         require.resolve('@babel/preset-es2015'),
-        require.resolve('@babel/preset-stage-0'),
+        [ require.resolve('@babel/preset-stage-0'), { decoratorsLegacy: true } ],
         [
             require.resolve('@babel/preset-env'),
             { 'targets': { 'browsers': [ 'android >= 4' ] } },
@@ -12,7 +12,7 @@ module.exports = {
     plugins: [
         // [ require.resolve('@babel/plugin-transform-react-jsx'), { pragma: 'h' } ],
         require.resolve('babel-plugin-transform-vue-jsx'),
-        require.resolve('@babel/plugin-proposal-decorators'),
+        [ require.resolve('@babel/plugin-proposal-decorators'), { legacy: true } ],
         [ require.resolve('@babel/plugin-proposal-class-properties'), { loose: true } ],
         require.resolve('@babel/plugin-proposal-optional-chaining'),
         require.resolve('@babel/plugin-proposal-nullish-coalescing-operator'),
