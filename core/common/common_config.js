e48609e32b505802fd62e10cf6da8e3d9ada1feb
lijialiang <lijialiang@yy.com>
2018-11-02 09:55:15 +0800

    feat: default alias for axios

--- core/common/common_config.js
+++ core/common/common_config.js
@@ -42,6 +42,7 @@ const resolve = (_config_) => {
   const workflowConfig = config[ `workflow.${workflow}` ] || { }
 
   const defaultAlias = {
+    'axios': `${projectPath}/node_modules/axios`,
     'moment': `${projectPath}/node_modules/moment`,
     'lodash': `${projectPath}/node_modules/lodash`,
     '@local': `${projectPath}/node_modules`,
